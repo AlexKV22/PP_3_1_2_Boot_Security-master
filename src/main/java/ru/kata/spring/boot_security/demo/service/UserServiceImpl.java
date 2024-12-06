5fe3812120803d69367c90587ee6ac21cf8eedc7
aleksandrkakovkin <sasha22.94@mail.ru>
2024-12-06 13:17:31 +0300

    Fix the code

--- src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
@@ -80,7 +80,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User findByUsername(String username) {
-        return userRepository.findByUsername(username).orElseThrow(()-> new UsernameNotFoundException(username));
+        return userRepository.findByUsername(username).orElseThrow();
     }
 
     @Override
